5fecda8da6ad9eb5b7d08dd43518f0ad457bbc80
Leslie <evil_outman@163.com>
2017-11-20 18:09:10 +0800

    begin vue Watcher

--- vue/响应式原理/observe.js
+++ vue/响应式原理/observe.js
@@ -1,54 +1,61 @@
-/*
-* 观察数据的方法，并且可以返回数据的观察者
-*/ 
-function observe(value) {
-	//如果数据不是对象就直接返回
-	if(typeof value !== 'object') return;
-	let ob;
+// /*
+// * 观察数据的方法，并且可以返回数据的观察者
+// */ 
+// function observe(value) {
+// 	//如果数据不是对象就直接返回
+// 	if(typeof value !== 'object') return;
+// 	let ob;
 
-	//如果数据有观察者，就直接返回观察者
-	if(value.__ob__){
-		ob = value.__ob__;
-	//如果数据没有观察者，就观察数据，新建一个观察者并且返回这个观察者	
-	}else if(typeof value === 'object'){
-		ob = new Observer(value);
-	}
+// 	//如果数据有观察者，就直接返回观察者
+// 	if(value.__ob__){
+// 		ob = value.__ob__;
+// 	//如果数据没有观察者，就观察数据，新建一个观察者并且返回这个观察者	
+// 	}else if(typeof value === 'object'){
+// 		ob = new Observer(value);
+// 	}
 
-	return ob;
-}
+// 	return ob;
+// }
 
-//观察者类
-class Observer {
-	constructor(value){
-		//如果是数组
-		if(Aarray.isArray(value)){
-			//使用变异的方法来替换某些原生的数组方法  如push、pop等
-			augment(value, arrayMethods, arrayKeys);
-		//这里没有进行值的判断
-		//但实际上只对对象起作用
-		}else{
+// //观察者类
+// class Observer {
+// 	constructor(value){
+// 		//如果是数组
+// 		if(Aarray.isArray(value)){
+// 			//使用变异的方法来替换某些原生的数组方法  如push、pop等
+// 			augment(value, arrayMethods, arrayKeys);
+// 		//这里没有进行值的判断
+// 		//但实际上只对对象起作用
+// 		}else{
 
-		}
-	}
-}
+// 		}
+// 	}
+// }
 
 function defineReactive(obj,key,val) {
 	const dep = new Dep();
+	const property = Object.getOwnPropertyDescriptor(obj, key);
+
+	const getter = property && property.get
+  	const setter = property && property.set
 
 	Object.defineProperty(obj,key,{
 		enumerable: true,
     	configurable: true,
     	get: function () {
+    		const value = getter ? getter.call(obj) : val
+
     		if(Dep.target){
     	        //添加订阅
     	        dep.depend()
     	    }
-    		return val;
+    		return value;
     	},
     	set: function (newVal) {
+    		const value = getter ? getter.call(obj) : val
     		//判断新值与旧值是否相等
     		//判断的后半段是为了验证新值与旧值都为NaN的情况  NaN不等于自身
-    		if(newVal === val || (newVal !== newVal && value !== value)){
+    		if(newVal === value || (newVal !== newVal && value !== value)){
     			return ;
     		}
     		// observe(newVal);
